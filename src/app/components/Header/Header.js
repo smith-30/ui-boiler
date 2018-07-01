f4bab494c52302fb447a06edd0222a122e26183d
misu <parkour12019@gmail.com>
2018-07-02 00:42:16 +0900

    mod: clean

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Banner from './images/banner.jpg';
+import Banner from './images/logo.png';
 import './style.scss';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="header">
-        <a href="https://twitter.com/flexdinesh">
+        <a href="https://github.com/smith-30">
           <img src={Banner} alt="react-redux-boilerplate - Logo" />
         </a>
-        <div className="nav-bar">
+        {/* <div className="nav-bar">
           <Link className="router-link" to="/">
             Home
           </Link>
           <Link className="router-link" to="/features">
             Features
           </Link>
-        </div>
+        </div> */}
       </div>
     );
   }
